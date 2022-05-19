99edd7873dfce4fba89ee56e1fc228bd4deb7470
Nicholas Suh <nicksuh17@gmail.com>
2022-05-18 21:50:24 -0700

    firestore read/write methods

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require('path');
 const server = http.createServer(app);
 
-port = 3000;
+port = 3000
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+  });
+  app.get('/smth', function(req, res){
+    res.sendFile(path.join(__dirname + '/pages/smth.html'));
+  });
+
 
 server.listen(port, ()=> {
     console.log("listening on *: " + port);
