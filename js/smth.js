ee27b07fbe71ab91b01163fb8e75ac7e66bfa384
Nicholas Suh <nicksuh17@gmail.com>
2022-05-19 18:34:34 -0700

    more database methods

--- js/smth.js
+++ js/smth.js
@@ -1,5 +1,64 @@
+/* FUNNY DOCUMENTATION
+
+Team document Fields
+- name: string
+- HP: double
+- damageDealt: double
+- period: double
+- slogan: string
+
+Alliance document fields
+- name: string
+- HP: double
+- damageDealt: double
+- Teams: subcollection of team documents
+
+addTeam(allianceName, teamName, inputHP, inputDamageDealt, inputPeriod, inputSlogan)
+- allianceName, teamName, inputSlogan are strings, rest are numbers
+- creates a new team with the inputted values
+- returns 1 if the alliance is not found
+
+delTeam(allianceName, teamName)
+- deletes the team named (teamName) that is a member of (allianceName)
+
+getTeam(allianceName, teamName)
+- gets the specified team
+- returns the team document as an object. u can access values with .HP, .damageDealt, etc.
+
+getAlliance(allianceName)
+- gets the specified alliance
+- returns the alliance document as an object.
+
+getTeamsInAlliance(allianceName)
+- returns all the teams in the specified alliance
+- returns 1 if alliance not found
+- return value is a querySnapshot. iterate through with .forEach or call the .doc property for an array of objects.
+- used in the calculate methods if u want examples on how to use
+
+getTeamsHPOrder(numTeams = -1)
+- returns a list of teams in HP order, greatest hp to least hp
+- returns all teams by default, paramter will specify how many teams to return
+- return value is a querySnapshot
+
+getTeamsDMGOrder(numTeams = -1)
+- similar to above but in damageDealt order
+
+getAlliancesHPOrder(numAlliances = -1)
+- similiar but alliance hp
+
+getAlliancesDMGOrder(numAlliances = -1)
+- similar but alliance damageDealt
+
+calcAllianceHP(allianceName)
+- updates the HP field of the specified alliance 
+- returns 1 if alliance not found
+
+calcAllianceDMG(allianceName)
+- same as above but for allianceDamage
+
+*/
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js';
-import { getFirestore, collection, getDocs, getDoc, setDoc, doc } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js'
+import { getFirestore, collection, deleteDoc, limit, getDocs, getDoc, setDoc, doc, query, where, collectionGroup, orderBy, updateDoc } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js'
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7zyFs0nG8lHlAa8BKcQaGxbZWlGZ7Hrk",
@@ -16,7 +75,6 @@ const db = getFirestore(app);
 
 //will overrite existing team, probably use this for updating as well
 async function addTeam( allianceName, teamName, inputHP, inputDamageDealt, inputPeriod, inputSlogan){
-    //client side alliance name check to avoid read pricing
     if(!(allianceName == "League of Fowls" || allianceName == "Aquatic Alliance" || allianceName == "Chitin Coalition" || allianceName == "Land Animals United") )
     {
         console.log("alliance not found")
@@ -25,11 +83,18 @@ async function addTeam( allianceName, teamName, inputHP, inputDamageDealt, input
 
     const teamRef = doc(db, "alliances", allianceName, "Teams", teamName)
     setDoc(teamRef,{
+        name: teamName,
         HP: inputHP,
         damageDealt: inputDamageDealt,
         period: inputPeriod,
         slogan: inputSlogan
     })
+}
+
+async function delTeam(allianceName, teamName)
+{
+    const teamRef = doc(db, "alliances", allianceName, "Teams", teamName)
+    await deleteDoc(teamRef);
 }
 
 async function getTeam(allianceName, teamName){
@@ -44,8 +109,159 @@ async function getTeam(allianceName, teamName){
     return teamSnap.data()
 }
 
+async function getAlliance(allianceName){
+    const allianceRef = doc(db, "alliances", allianceName);
+    const allianceSnap = await getDoc(allianceRef)
 
-addTeam("Land Animals United", "female lovers8379", 2, 11 ,1 ,"We love females")
+    if(!allianceSnap.exists())
+    {
+        console.log("Alliance not founds")
+        return 1;
+    }
+    return allianceSnap.data();
+}
+
+async function getTeamsInAlliance(allianceName){
+    if(!(allianceName == "League of Fowls" || allianceName == "Aquatic Alliance" || allianceName == "Chitin Coalition" || allianceName == "Land Animals United") )
+    {
+        console.log("alliance not found")
+        return 1;
+    }
+    const q = query(collection(db, "alliances", allianceName, "Teams"));
+    const querySnapshot = await getDocs(q)
+    return querySnapshot
+}
+
+//use .docs to get array of documents, or iterate with forEach
+async function getTeamsHPOrder(numTeams = -1){
+    let teams
+    if(numTeams == -1)
+    {
+        teams = query(collectionGroup(db, "Teams"), orderBy("HP", "desc"));
+    }
+    else
+    {
+        teams = query(collectionGroup(db, "Teams"), orderBy("HP", "desc"), limit(numTeams));
+    }
+    const teamSnapshot = await getDocs(teams);
+    // teamSnapshot.forEach((doc) => {
+    //     console.log(doc.data());
+    // })
+    return teamSnapshot; 
+}
+
+async function getTeamsDMGOrder(numTeams = -1){
+    let teams
+    if(numTeams == -1)
+    {
+        teams = query(collectionGroup(db, "Teams"), orderBy("damageDealt", "desc"));
+    }
+    else
+    {
+        teams = query(collectionGroup(db, "Teams"), orderBy("damageDealt", "desc"), limit(numTeams));
+    }
+    const teamSnapshot = await getDocs(teams);
+    // teamSnapshot.forEach((doc) => {
+    //     console.log(doc.data());
+    // })
+    return teamSnapshot; 
+}
+
+async function getAlliancesHPOrder(numAlliances = -1){
+    let alliances
+    if(numAlliances == -1)
+    {
+        alliances = query(collection(db, "alliances"), orderBy("HP", "desc"));
+    }
+    else
+    {
+        alliances = query(collection(db, "alliances"), orderBy("HP", "desc"), limit(numAlliances));
+    }
+    const alliancesSnapshot = await getDocs(alliances);
+    // teamSnapshot.forEach((doc) => {
+    //     console.log(doc.data());
+    // })
+    return alliancesSnapshot; 
+}
+
+async function getAlliancesDMGOrder(numAlliances = -1){
+    let alliances
+    if(numAlliances == -1)
+    {
+        alliances = query(collection(db, "alliances"), orderBy("damageDealt", "desc"));
+    }
+    else
+    {
+        alliances = query(collection(db, "alliances"), orderBy("damageDealt", "desc"), limit(numAlliances));
+    }
+    const alliancesSnapshot = await getDocs(alliances);
+    // teamSnapshot.forEach((doc) => {
+    //     console.log(doc.data());
+    // })
+    return alliancesSnapshot; 
+}
+
+async function calcAllianceHP(allianceName){
+    let teams = await getTeamsInAlliance(allianceName)
+    if(teams != 1)
+    {
+        let sum = 0;
+        teams.forEach((doc) =>{
+            sum += doc.data().HP
+        })
+        const allianceRef = doc(db, "alliances", allianceName)
+        updateDoc(allianceRef,{
+            HP: sum
+        })
+    }
+    else
+    {
+        return 1
+    }
+}
+
+async function calcAllianceDMG(allianceName){
+    let teams = await getTeamsInAlliance(allianceName)
+    if(teams != 1)
+    {
+        let sum = 0;
+        teams.forEach((doc) =>{
+            sum += doc.data().damageDealt
+        })
+        const allianceRef = doc(db, "alliances", allianceName)
+        updateDoc(allianceRef,{
+            damageDealt: sum
+        })
+    }
+    else
+    {
+        return 1
+    }
+}
+await calcAllianceHP("Aquatic Alliance");
+addTeam("Land Animals United", "female lovers8379", 2, 110 ,1 ,"We love females")
 const result = await getTeam("Land Animals United", "female lovers8379")
-console.log(result )
+const result1 = await getAlliance("League of Fowls");
+const result2 = await getTeamsHPOrder(3)
+const result3 = await getTeamsDMGOrder()
+
+await delTeam("Land Animals United", "female lovers8379")
+const result4 = await getTeamsDMGOrder();
+const result5 = await getAlliancesDMGOrder();
+const result6 = await getTeamsInAlliance("Chitin Coalition")
+// result2.forEach((doc) => {
+//     console.log(doc.data());
+// })
+// result3.forEach((doc) => {
+//     console.log(doc.data());
+// })
+// result4.forEach((doc) => {
+//     console.log(doc.data());
+// })
+// result5.forEach((doc) => {
+//     console.log(doc.data());
+// })
+result5.forEach((doc) => {
+    console.log(doc.data());
+})
 
